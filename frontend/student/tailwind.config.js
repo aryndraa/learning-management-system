4870c3b832ecb477a6c5f09541ac022971351672
aryndraa <aryacoder1102@gmail.com>
2025-01-01 22:26:45 +0800

    frontend - set up student app

---
+++ frontend/student/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+
+      // fonts
+      fontFamily: {
+        poppins: '"Poppins", sans-serif'
+      },
+
+      // colors
+      colors: {
+        primary : '#5A2BBF',
+        font : {
+          100 : "#929292",
+          200 : "#4F4F4F"
+        },
+        success : "#0FD45B",
+        warning : "#FFD220",
+        fatal   : "#D9333D",
+        background : "#F9F9F9"
+      }
+    },
+  },
+  plugins: [],
+}
