e4a4b061a31d53ab6bbab8f0343709561edc647d
aryndraa <aryacoder1102@gmail.com>
2025-01-01 23:55:27 +0800

    frontend - tablet display header for login page student

--- frontend/student/src/components/Logo.jsx
+++ frontend/student/src/components/Logo.jsx
@@ -2,9 +2,9 @@ const Logo = () => {
   return (
     <>
       <div className="flex gap-2 items-center ">
-        <img src="/logo.svg" alt="logo app" className="w-7 2xl:w-10"/>
+        <img src="/logo.svg" alt="logo app" className="w-7 md:w-10"/>
         <strong>
-          <h1 className="text-xl 2xl:text-[28px] text-font-300">
+          <h1 className="text-xl md:text-[28px] text-font-300">
             Onclass<span className="text-primary">.</span>
           </h1>
         </strong>
--- frontend/student/src/components/header/LanguageDropdown.jsx
+++ frontend/student/src/components/header/LanguageDropdown.jsx
@@ -5,10 +5,10 @@ const LanguageDropdown = () => {
   return (
     <>
       <div>
-        <div className="flex items-center gap-1 2xl:gap-2 text-font-300">
-          <FiGlobe className="text-lg 2xl:text-2xl"/>
-          <p className="text-sm 2xl:text-lg font-medium">En</p>
-          <FaChevronDown  className="hidden lg:block"/>
+        <div className="flex items-center gap-1 md:gap-2 text-font-300">
+          <FiGlobe className="text-lg md:text-2xl"/>
+          <p className="text-sm md:text-lg font-medium">En</p>
+          <FaChevronDown  className="hidden md:block"/>
         </div>
       </div>
     </>
