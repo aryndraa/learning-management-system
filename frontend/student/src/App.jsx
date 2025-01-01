b30b21f45452ef443fe4b4d0bf74d1ca93940e5b
aryndraa <aryacoder1102@gmail.com>
2025-01-01 23:11:49 +0800

    frontend - authentication middleware in student app

--- frontend/student/src/App.jsx
+++ frontend/student/src/App.jsx
@@ -1,8 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from "./pages/auth/Login.jsx"
+import AuthMiddleware from "./middlewares/AuthMiddleware.jsx";
+import {Home} from "./pages/Home.jsx";
+
 function App() {
 
   return (
     <>
-
+      <Router>
+        <AuthMiddleware isAuth={false} excludeRoutes={['/login']}>
+          <Routes>
+            <Route path="/login" element={<Login/>} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </AuthMiddleware>
+      </Router>
     </>
   )
 }
