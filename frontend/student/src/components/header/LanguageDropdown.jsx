2e3db7ed2d1515397ac43521ca9d25252a078b38
aryndraa <aryacoder1102@gmail.com>
2025-01-02 00:00:20 +0800

    frontend - remove  state for language

--- frontend/student/src/components/header/LanguageDropdown.jsx
+++ frontend/student/src/components/header/LanguageDropdown.jsx
@@ -1,16 +1,13 @@
 import { FiGlobe } from "react-icons/fi";
 import { FaChevronDown } from "react-icons/fa";
-import {useState} from "react";
 
 const LanguageDropdown = () => {
-  const [language, setLanguage] = useState("en");
-
   return (
     <>
       <div>
         <div className="flex items-center gap-1 md:gap-2 text-font-300">
           <FiGlobe className="text-lg md:text-2xl"/>
-          <p className="text-sm md:text-lg font-medium capitalize">{language}</p>
+          <p className="text-sm md:text-lg font-medium">En</p>
           <FaChevronDown  className="hidden md:block"/>
         </div>
       </div>
