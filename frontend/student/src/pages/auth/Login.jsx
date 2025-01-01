4c8746dd1c3ce6b369e14d50bca9692fe4872afa
aryndraa <aryacoder1102@gmail.com>
2025-01-01 23:57:51 +0800

    frontend - helmet title

--- frontend/student/src/pages/auth/Login.jsx
+++ frontend/student/src/pages/auth/Login.jsx
@@ -1,8 +1,12 @@
 import {AuthHeader} from "../../components/header/AuthHeader.jsx";
+import {Helmet} from "react-helmet";
 
 export default function Login() {
   return(
     <>
+      <Helmet>
+        <title>LMS - Student Login</title>
+      </Helmet>
       <AuthHeader/>
       <h1>Login</h1>
     </>
