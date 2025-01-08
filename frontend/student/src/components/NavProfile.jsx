6c1bd60c46648f6d3b01cefa062d698278b80e75
aryndraa <aryacoder1102@gmail.com>
2025-01-08 23:01:10 +0800

    frontend - modal skeleton

--- frontend/student/src/components/NavProfile.jsx
+++ frontend/student/src/components/NavProfile.jsx
@@ -35,9 +35,10 @@ export const NavProfile = () => {
     <>
       {loading ?
 
-        <div className="pl-8 flex items-center gap-2">
-          <span className="skeleton rounded-full w-48 h-[46px]"></span>
-          <span className="skeleton rounded-full w-[46px] h-[46px]"></span>
+        <div className="ml-8 flex items-center gap-2 bg-neutral-100 py-2 px-2 rounded-full">
+          <span className="bg-neutral-200 rounded-full w-44 h-[46px]"></span>
+          <span className="bg-neutral-200 rounded-full w-[46px] h-[46px]"></span>
+          <span className="bg-neutral-200 rounded-full w-[46px] h-[46px]"></span>
         </div>
 
         :
