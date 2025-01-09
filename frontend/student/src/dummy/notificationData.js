6160de810b4b5b02633a1b30216ea2c52c8ad771
aryndraa <aryacoder1102@gmail.com>
2025-01-09 23:04:47 +0800

    frontend - notification dummy data

---
+++ frontend/student/src/dummy/notificationData.js
@@ -0,0 +1,18 @@
+const notificationData = [
+  {
+    title: "Notification 1",
+    description: "This is a notification",
+    type: "warning",
+    notifType: "assigments",
+    notifId: 1
+  },
+  {
+    title: "Notification 1",
+    description: "This is a notification",
+    type: "warning",
+    notifType: "announcement",
+    notifId: 2
+  }
+]
+
+export default notificationData
