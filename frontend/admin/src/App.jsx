e5a2ea4394a8eb14a1507f510aeb18955b521762
aryndraa <aryacoder1102@gmail.com>
2025-01-18 23:31:22 +0800

    frontend - middleware auth admin

--- frontend/admin/src/App.jsx
+++ frontend/admin/src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AuthMiddleware from "./middlewares/AuthMiddleware.jsx";
+
 function App() {
+
+  const hasLogin = false
 
   return (
     <>
+      <Router>
+        <AuthMiddleware isAuth={hasLogin} excludeRoutes={['/auth/login']}  >
+          <Routes>
+          {/*  page */}
+          </Routes>
+        </AuthMiddleware>
+      </Router>
     </>
   )
 }
---
+++ frontend/admin/src/middlewares/AuthMiddleware.jsx
@@ -0,0 +1,17 @@
+import {useEffect} from 'react'
+import { useNavigate } from 'react-router'
+
+const AuthMiddleware = ({ isAuth, children, excludeRoutes = []}) => {
+    const navigate = useNavigate();
+    const currentPath = window.location.pathname;
+
+    useEffect(() => {
+        if (!isAuth && !excludeRoutes.includes(currentPath)) {
+            navigate('auth/login');
+        }
+    }, [isAuth, excludeRoutes, currentPath]);
+
+    return children;
+}
+
+export default AuthMiddleware
