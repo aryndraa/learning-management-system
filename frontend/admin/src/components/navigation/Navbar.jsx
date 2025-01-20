2d20da1e982e43d2808588747d07e73e09f34497
aryndraa <aryacoder1102@gmail.com>
2025-01-20 22:59:38 +0800

    frontend - side active state

--- frontend/admin/src/components/navigation/Navbar.jsx
+++ frontend/admin/src/components/navigation/Navbar.jsx
@@ -1,32 +1,30 @@
 import Logo from "./../Logo"
 import { Link } from "react-router-dom"
-import { IoIosMenu } from "react-icons/io";
-import { IoClose } from "react-icons/io5";
+
 import { TfiMenu } from "react-icons/tfi";
 
-import Navigation from "./Navigation";
+import Sidebar from "./Sidebar";
+import { useState } from "react";
 
 const Navbar = () => {
+
+  const [sideActive, setSideActive] = useState(false)
+
   return (
     <>
       <div className="bg-white flex justify-between items-center  px-5 md:px-12 xl:px-24 py-5 md:py-8 relative">
-          <button className="text-2xl absolute top-1/2 transform -translate-y-1/2">
+          <button 
+            className="text-2xl absolute top-1/2 transform -translate-y-1/2"
+            onClick={() => {setSideActive(!sideActive)}}
+          >
             <TfiMenu/>
           </button>
           <div className="m-auto">
               <Logo/>
           </div>
       </div>
-      <div className="bg-white fixed left-0 top-0 bottom-0 w-[80%] rounded-lg py-5 ">
-        <button className="flex justify-between items-center text-xl gap-1 font-medium px-4 mb-8">
-          <span className="text-3xl"> 
-            <IoClose/>
-          </span>
-          Menu
-        </button>
-        
-        <Navigation/>
-      </div>
+
+      <Sidebar isActive={sideActive} setSideActive={setSideActive}/>
     </>
   )
 }
---
+++ frontend/admin/src/components/navigation/Sidebar.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { IoClose } from "react-icons/io5";
+import Navigation from "./Navigation";
+
+const Sidebar = ({ isActive = false, setSideActive }) => {
+
+
+    return (
+        <div>
+            <div 
+                className={`min-h-screen w-full fixed top-0 left-0 bottom-0 right-0 bg-font-100/40 ${
+                    isActive ? "block" : "hidden"
+                }`}
+                onClick={() => { setSideActive(!isActive) }}
+            ></div>
+            <div className={`bg-white fixed left-0 top-0 bottom-0 w-[80%] rounded-lg py-5 transform transition duration-500 ease-in-out ${
+                isActive ? "-translate-x-0" : "-translate-x-[100%]"
+            }`}>
+                <button 
+                    className="flex justify-between items-center text-xl gap-1 font-medium px-4 mb-8"
+                    onClick={() => { setSideActive(!isActive) }}
+                >
+                  <span className="text-3xl"> 
+                    <IoClose/>
+                  </span>
+                  Menu
+                </button>
+
+                <Navigation/>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
