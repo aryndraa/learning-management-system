d251f219771582921722785c02ecc74eb2829287
aryndraa <aryacoder1102@gmail.com>
2025-01-21 22:43:35 +0800

    frontend - drawer

--- frontend/admin/src/components/navigation/Navbar.jsx
+++ frontend/admin/src/components/navigation/Navbar.jsx
@@ -12,18 +12,17 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="bg-white flex justify-between items-center  px-5 md:px-12 xl:px-24 py-5 md:py-8 relative">
+      <div className="bg-white flex justify-between items-center lg:items-start px-5 md:px-12 xl:px-14 py-5 md:py-6 relative lg:w-fit lg:fixed top-0 left-0 bottom-0 lg:m-4 lg:rounded-lg lg:shadow ">
           <button 
-            className="text-2xl absolute top-1/2 transform -translate-y-1/2"
+            className="text-2xl absolute top-1/2 transform -translate-y-1/2 lg:hidden"
             onClick={() => {setSideActive(!sideActive)}}
-          >
+            >
             <TfiMenu/>
           </button>
-          <div className="m-auto">
+          <div className="m-auto lg:m-0">
               <Logo/>
           </div>
       </div>
-
       <Sidebar isActive={sideActive} setSideActive={setSideActive}/>
     </>
   )
--- frontend/admin/src/components/navigation/Sidebar.jsx
+++ frontend/admin/src/components/navigation/Sidebar.jsx
@@ -6,18 +6,20 @@ const Sidebar = ({ isActive = false, setSideActive }) => {
 
 
     return (
-        <div>
+        <div className="lg:hidden">
             <div 
                 className={`min-h-screen w-full fixed top-0 left-0 bottom-0 right-0 bg-font-100/40 ${
                     isActive ? "block" : "hidden"
                 }`}
                 onClick={() => { setSideActive(!isActive) }}
             ></div>
-            <div className={`bg-white fixed left-0 top-0 bottom-0 w-[100%] rounded-xl py-5 transform transition duration-500 ease-in-out ${
-                isActive ? "translate-y-[20%]" : "translate-y-[100%]"
+
+            
+            <div className={`bg-white  fixed left-0 top-0 bottom-0 w-[100%]  rounded-xl py-5 transform  transition duration-500 ease-in-out ${
+                isActive ? "translate-y-[20%]" : "translate-y-[100%] "
             }`}>
                 <button 
-                    className="flex justify-between items-center text-xl gap-1 font-medium px-4 mb-8"
+                    className="flex justify-between items-center text-xl gap-1 font-medium px-4 mb-8 "
                     onClick={() => { setSideActive(!isActive) }}
                 >
                   <span className="text-3xl"> 
