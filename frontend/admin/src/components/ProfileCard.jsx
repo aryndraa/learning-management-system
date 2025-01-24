980a127ed6d1f4fa8886884cff579e2c20ae0ee3
aryndraa <aryacoder1102@gmail.com>
2025-01-24 21:12:28 +0800

    frontend - Admin : profile option modal state

--- frontend/admin/src/components/ProfileCard.jsx
+++ frontend/admin/src/components/ProfileCard.jsx
@@ -1,8 +1,12 @@
 import { IoMdMore } from "react-icons/io";
 import {Link} from "react-router-dom";
 import { FiUser, FiLogOut, FiInfo   } from "react-icons/fi";
+import {useState} from "react";
 
 export const ProfileCard = () => {
+
+  const [modalActive, setModalActive] = useState(false);
+
   return (
     <>
       <div className="flex justify-between items-center p-4 bg-white rounded-lg relative">
@@ -17,11 +21,19 @@ export const ProfileCard = () => {
             <h2 className="text-font-200">Admin Slebew</h2>
           </div>
         </div>
+
         <div className="flex items-center gap-2">
-          <button className="text-2xl p-2 border-font-100 border rounded-full text-font-100 ">
+          <button
+            className="text-2xl p-2 border-font-100 border rounded-full text-font-100 "
+            onClick={() => setModalActive(!
+              modalActive)}
+          >
             <IoMdMore />
           </button>
-          <div className="bg-white flex flex-col absolute right-0 left-0 transform translate-y-32 shadow-md rounded-lg">
+
+          <div className={`bg-white flex-col absolute right-0 left-0 transform translate-y-32 shadow-md rounded-lg ${
+            modalActive ? "flex" : "hidden"
+          }`}>
             <Link to={'/profile'} className="py-4 px-4  hover:bg-background flex items-center gap-4 rounded-t-lg">
               <span className="text-xl"><FiUser/></span>
               Profile
