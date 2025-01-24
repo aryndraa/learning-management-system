18ea413db0ccf49b3e6bce871a7c363a01faff57
aryndraa <aryacoder1102@gmail.com>
2025-01-24 21:09:15 +0800

    frontend - Admin : profile option modal

--- frontend/admin/src/components/ProfileCard.jsx
+++ frontend/admin/src/components/ProfileCard.jsx
@@ -1,9 +1,11 @@
 import { IoMdMore } from "react-icons/io";
+import {Link} from "react-router-dom";
+import { FiUser, FiLogOut, FiInfo   } from "react-icons/fi";
 
 export const ProfileCard = () => {
   return (
     <>
-      <div className="flex justify-between items-center p-4 bg-white rounded-lg">
+      <div className="flex justify-between items-center p-4 bg-white rounded-lg relative">
         <div className="flex items-center gap-4">
           <img
             src="http://127.0.0.1:8000/storage/teacher/avatars/1MNtbj4qcgARkmKWpQh0cAgFJAwJRqPQGDfGT5O4.jpg"
@@ -16,9 +18,23 @@ export const ProfileCard = () => {
           </div>
         </div>
         <div className="flex items-center gap-2">
-          <button className="text-2xl p-2 border-font-200 border rounded-full text-font-200 ">
+          <button className="text-2xl p-2 border-font-100 border rounded-full text-font-100 ">
             <IoMdMore />
           </button>
+          <div className="bg-white flex flex-col absolute right-0 left-0 transform translate-y-32 shadow-md rounded-lg">
+            <Link to={'/profile'} className="py-4 px-4  hover:bg-background flex items-center gap-4 rounded-t-lg">
+              <span className="text-xl"><FiUser/></span>
+              Profile
+            </Link>
+            <Link to={'/options'} className="py-4 px-4  hover:bg-background flex items-center gap-x-4">
+              <span className="text-xl"><FiInfo/></span>
+              Options
+            </Link>
+            <button className="text-left py-4 px-4 hover:bg-background flex items-center gap-4 rounded-b-lg">
+              <span className="text-xl"><FiLogOut/></span>
+              Log Out
+            </button>
+          </div>
         </div>
       </div>
     </>
