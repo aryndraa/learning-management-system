f869e9f3e67146ff2bd69345fd96c8849930e8b3
aryndraa <aryacoder1102@gmail.com>
2025-01-26 22:34:06 +0800

    backend - total items today

--- backend/app/Http/Controllers/Api/V1/Admin/Summary/SummaryController.php
+++ backend/app/Http/Controllers/Api/V1/Admin/Summary/SummaryController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Api\V1\Admin\Summary;
 
 use App\Http\Controllers\Controller;
+use App\Models\Assignment;
 use App\Models\Classroom;
+use App\Models\Material;
+use App\Models\Meeting;
+use App\Models\Teacher;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
@@ -11,12 +15,27 @@ class SummaryController extends Controller
 {
     public function todaySummary()
     {
-        $totalClass = Classroom::query()
-            ->where('created_at', Carbon::now('Asia/Jakarta'))
+        $today = Carbon::now('Asia/Jakarta');
+
+        $totalClass = Meeting::query()
+            ->where('created_at', $today)
             ->count();
 
+        $totalMaterials = Material::query()
+            ->where('created_at', $today)
+            ->count();
+
+        $totalAssigemnts = Assignment::query()
+            ->where('created_at', $today)
+            ->count();
+
+
         return response()->json([
-            "data" => $totalClass
+            "data" => [
+                "total_classrooms" => $totalClass,
+                "total_materials" => $totalMaterials,
+                "total_assigments" => $totalAssigemnts,
+            ]
         ]);
     }
 }
