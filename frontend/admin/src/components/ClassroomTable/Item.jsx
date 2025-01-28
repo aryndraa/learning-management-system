73b6040edfe25b2b5a52d85983fcd59faba96411
aryndraa <aryacoder1102@gmail.com>
2025-01-29 03:11:30 +0800

    frontend - classroom table item

---
+++ frontend/admin/src/components/ClassroomTable/Item.jsx
@@ -0,0 +1,33 @@
+export const Item = () => {
+  return (
+    <>
+      <div className="border-b last:border-b-transparent pb-4 mb-4 border-border">
+        <div className=" rounded-lg  flex gap-4 p-2">
+          <div className="flex gap-4 ">
+            <div className="flex flex-col gap-2">
+              <h4 className="font-medium text-font-200">Class</h4>
+              <h4 className="font-medium text-font-200">Major</h4>
+              <h4 className="font-medium text-font-200">Number</h4>
+              <h4 className="font-medium text-font-200">Teacher</h4>
+              <h4 className="font-medium text-font-200">Status</h4>
+            </div>
+            <div className="flex flex-col gap-2">
+              <p>:</p>
+              <p>:</p>
+              <p>:</p>
+              <p>:</p>
+              <p>:</p>
+            </div>
+          </div>
+          <div className="flex-1 flex flex-col gap-2">
+            <p className="text-font-100">Soul</p>
+            <p className="text-font-100">Web Development</p>
+            <p className="text-font-100">12</p>
+            <p className="text-font-100">Pak Slebew</p>
+            <p className="text-font-100">Online</p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
--- frontend/admin/src/pages/classroom/Classroom.jsx
+++ frontend/admin/src/pages/classroom/Classroom.jsx
@@ -1,7 +1,17 @@
+import {PageTitle} from "../../components/PageTitle.jsx";
+import {Item} from "../../components/ClassroomTable/Item.jsx";
+
 export default function Classroom () {
   return (
     <>
-        <h1>Classroom</h1>
+      <section className="grid grid-cols-1 gap-4 w-full ">
+        <PageTitle title="Classroom Management" />
+        <div className="grid grid-cols-1 gap-4 w-full bg-white p-4">
+          <Item/>
+          <Item/>
+          <Item/>
+        </div>
+      </section>
     </>
   )
 }
