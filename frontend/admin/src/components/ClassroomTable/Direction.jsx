548b7eae4aec3b4596de89510f904b0bced13ea1
aryndraa <aryacoder1102@gmail.com>
2025-01-30 23:31:38 +0800

    frontend - direction button toggle

--- frontend/admin/src/components/ClassroomTable/Direction.jsx
+++ frontend/admin/src/components/ClassroomTable/Direction.jsx
@@ -3,17 +3,27 @@ import {useClassroom} from "../../contexts/ClassroomContext.jsx";
 
 export const Direction = () => {
   const {direction, setDirection} = useClassroom();
+
+  const handleToggle = () => {
+    if (direction === "asc") {
+      return (
+        <button className="p-3 text-xl bg-neutral-100 rounded-lg text-font-100 flex gap-2 items-center font-medium">
+          <PiSortAscendingBold/>
+        </button>
+      )
+    } else if (direction === "desc") {
+      return (
+        <button className="p-3 text-xl bg-neutral-100 rounded-lg text-font-100 flex gap-2 items-center font-medium">
+          <PiSortDescendingBold/>
+        </button>
+      )
+    }
+  }
+
+  console.log(direction);
   return (
     <>
-      <button className="p-3 text-xl bg-neutral-100 rounded-lg text-font-100 flex gap-2 items-center font-medium">
-        {
-          direction === "asc" ? (
-            <PiSortAscendingBold/>
-          ) : (
-            <PiSortDescendingBold/>
-          )
-        }
-      </button>
+      {handleToggle()}
     </>
   )
 }
