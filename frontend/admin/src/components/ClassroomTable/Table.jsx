de5931f0f888f4e05a37fdaa17fe0a8a643d95f6
aryndraa <aryacoder1102@gmail.com>
2025-01-31 23:10:52 +0800

    frontend - order direction

--- frontend/admin/src/components/ClassroomTable/Table.jsx
+++ frontend/admin/src/components/ClassroomTable/Table.jsx
@@ -1,18 +1,66 @@
 import { Link } from "react-router-dom"
+import {useClassroom} from "../../contexts/ClassroomContext.jsx";
+import {PiSortAscendingBold, PiSortDescendingBold} from "react-icons/pi";
 
 const Table = () => {
+
+  const {orderBy, setOrderBy, direction, setDirection} = useClassroom();
+
+  const handleOrderDirection = (newOrderBy) => {
+    if (orderBy === newOrderBy) {
+      setDirection((prevDirection) => (prevDirection === "asc" ? "desc" : "asc"));
+    } else {
+      setOrderBy(newOrderBy);
+      setDirection("asc");
+    }
+  };
+
+  const orderDirectionButton = (column) => {
+    return (
+      <button className="text-xl" onClick={() => handleOrderDirection(column)}>
+        {orderBy === column && direction === "asc" ? <PiSortAscendingBold /> : <PiSortDescendingBold />}
+      </button>
+    );
+  };
+
+
   return (
     <>
-        <table className="table  rounded-lg bg-white">
+        <table className="table  rounded-lg bg-white overflow-scroll">
             <thead>
                 <tr className="text-white bg-primary rounded-lg  border-transparent">
                     <th className="font-medium text-sm rounded-tl-lg">#</th>
-                    <th className="font-medium text-sm">Class</th>
-                    <th className="font-medium text-sm">Major</th>
-                    <th className="font-medium text-sm">Number</th>
-                    <th className="font-medium text-sm">Teacher</th>
-                    <th className="font-medium text-sm">Student</th>
-                    <th className="font-medium text-sm rounded-tr-lg"></th>
+                    <th className="font-medium text-sm">
+                      <div className=" flex gap-2 items-center">
+                        Class
+                        {orderDirectionButton("classroom")}
+                      </div>
+                    </th>
+                    <th className="font-medium text-sm">
+                      <div className=" flex gap-2 items-center">
+                        Major
+                        {orderDirectionButton("major")}
+                      </div>
+                    </th>
+                    <th className="font-medium text-sm ">
+                      <div className="flex gap-2 items-center">
+                        Number
+                        {orderDirectionButton("number")}
+                      </div>
+                    </th>
+                    <th className="font-medium text-sm">
+                      <div className="flex gap-2 items-center">
+                        Teacher
+                        {orderDirectionButton("teacher")}
+                      </div>
+                    </th>
+                    <th className="font-medium text-sm">
+                      <div className="flex gap-2 items-center">
+                        Student
+                        {orderDirectionButton("student")}
+                      </div>
+                    </th>
+                    <th className="font-medium text-sm  rounded-tr-lg"></th>
                 </tr>
             </thead>
             <tbody>
