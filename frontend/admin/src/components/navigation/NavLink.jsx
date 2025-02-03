e78200e155bbd48d35263641f71d86d033a03a36
aryndraa <aryacoder1102@gmail.com>
2025-02-03 22:58:14 +0800

    frontend - dynamic navigation with parent path

--- frontend/admin/src/components/navigation/NavLink.jsx
+++ frontend/admin/src/components/navigation/NavLink.jsx
@@ -5,9 +5,13 @@ const NavLink = ({ link, isClose = false }) => {
 
     const location = useLocation()
 
+    const isActive = link.path === "/"
+      ? location.pathname === "/"
+      : location.pathname.startsWith(link.path);
+
     return (
       <Link to={link.path} className={`px-3 md:px-4 py-4 my-2 flex items-center gap-3 text-base lg:text-lg rounded-lg lg:rounded-full font-medium transition ease-in-out duration-300  ${
-        location.pathname === link.path ? "bg-primary text-white" : "bg-transparent text-font-100"
+        isActive  ? "bg-primary text-white" : "bg-transparent text-font-100"
       }`}>
           <span className="text-2xl">{link.icon}</span>
           <span className={`${isClose ? "hidden" : "block"}`}>
