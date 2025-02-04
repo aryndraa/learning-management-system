40b6b7c30f60a8c1b1349e68ef03df802adda20e
aryndraa <aryacoder1102@gmail.com>
2025-02-04 22:56:12 +0800

    frontend - get classroom id from params

--- frontend/admin/src/pages/classroom/ShowClassroom.jsx
+++ frontend/admin/src/pages/classroom/ShowClassroom.jsx
@@ -1,5 +1,14 @@
+import {PageTitle} from "../../components/PageTitle.jsx";
+import {useParams} from "react-router";
+
 export function ShowClassroom() {
+  const {classroom} = useParams();
+  console.log(classroom);
+
   return (
-    <></>
+    <>
+      <PageTitle title="Classroom Management" />
+
+    </>
   )
 }
