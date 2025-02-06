5dcc62b55cddcf17930b8a66ee8d7aba28c8f318
aryndraa <aryacoder1102@gmail.com>
2025-02-06 22:57:19 +0800

    frontend - class current activity information

---
+++ frontend/admin/src/components/Classroom/Overview/MeetingInformation.jsx
@@ -0,0 +1,13 @@
+export const MeetingInformation = () => {
+  return (
+    <>
+      <div className="bg-white p-4">
+        <h2 className="font-medium text-font-100 mb-3">Current Activity</h2>
+        <div className="mb-4">
+          <h3 className="text-xl font-medium text-font-300">Meeting Class</h3>
+          <p className="text-font-100">Bahasa Indonesia</p>
+        </div>
+      </div>
+    </>
+  )
+}
--- frontend/admin/src/pages/classroom/ShowClassroom.jsx
+++ frontend/admin/src/pages/classroom/ShowClassroom.jsx
@@ -1,5 +1,6 @@
-import { DetailOverview } from "../../components/Classroom/overview/DetailOverview.jsx";
+import { DetailOverview } from "../../components/Classroom/Overview/DetailOverview.jsx";
 import {PageTitle} from "../../components/PageTitle.jsx";
+import {MeetingInformation} from "../../components/Classroom/Overview/MeetingInformation.jsx";
 
 export function ShowClassroom() {
   return (
@@ -7,6 +8,7 @@ export function ShowClassroom() {
       <section className="grid grid-cols-1 gap-4 w-full ">
         <PageTitle title="Classroom Management" />
         <DetailOverview/>
+        <MeetingInformation/>
       </section>
     </>
   )
