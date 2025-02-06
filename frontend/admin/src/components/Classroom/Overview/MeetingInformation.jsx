1cd486e41faa5edd5624f74e490fef9bc5300d60
aryndraa <aryacoder1102@gmail.com>
2025-02-06 23:03:30 +0800

    frontend - activity status

--- frontend/admin/src/components/Classroom/Overview/MeetingInformation.jsx
+++ frontend/admin/src/components/Classroom/Overview/MeetingInformation.jsx
@@ -3,9 +3,15 @@ export const MeetingInformation = () => {
     <>
       <div className="bg-white p-4">
         <h2 className="font-medium text-font-100 mb-3">Current Activity</h2>
-        <div className="mb-4">
-          <h3 className="text-xl font-medium text-font-300">Meeting Class</h3>
-          <p className="text-font-100">Bahasa Indonesia</p>
+        <div className="mb-4 flex justify-between items-end">
+          <div>
+            <h3 className="text-xl font-medium text-font-300">Meeting Class</h3>
+            <p className="text-font-100">Bahasa Indonesia</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <div className="w-4 h-4 bg-success rounded-full"></div>
+            <span className=" font-medium text-font-100">On Going</span>
+          </div>
         </div>
         <div className="flex justify-between items-center gap-6 bg-primary/20 p-4 rounded-lg">
           <div className="bg-primary text-2xl font-medium text-white rounded-lg flex justify-center p-5 py-4">
