3be91a629fc6ebe858a483c337172379585de3b5
aryndraa <aryacoder1102@gmail.com>
2025-02-06 22:57:35 +0800

    frontend - activity duration

--- frontend/admin/src/components/Classroom/Overview/MeetingInformation.jsx
+++ frontend/admin/src/components/Classroom/Overview/MeetingInformation.jsx
@@ -7,6 +7,19 @@ export const MeetingInformation = () => {
           <h3 className="text-xl font-medium text-font-300">Meeting Class</h3>
           <p className="text-font-100">Bahasa Indonesia</p>
         </div>
+        <div className="flex justify-between items-center gap-6 bg-primary/20 p-4 rounded-lg">
+          <div className="bg-primary text-2xl font-medium text-white rounded-lg flex justify-center p-5 py-4">
+            <span>01</span>
+          </div>
+          <span className="text-3xl font-medium text-primary">:</span>
+          <div className="bg-primary text-2xl font-medium text-white rounded-lg flex justify-center p-5 py-4">
+            <span>20</span>
+          </div>
+          <span className="text-3xl font-medium text-primary">:</span>
+          <div className="bg-primary text-2xl font-medium text-white rounded-lg flex justify-center p-5 py-4">
+            <span>12</span>
+          </div>
+        </div>
       </div>
     </>
   )
