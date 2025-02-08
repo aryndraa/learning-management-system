b70162c3408214735dd8fdcd3c49e6683dbf93ff
aryndraa <aryacoder1102@gmail.com>
2025-02-08 21:19:15 +0800

    frontend - table absent mobile

--- frontend/admin/src/components/Classroom/Overview/TableAbsent.jsx
+++ frontend/admin/src/components/Classroom/Overview/TableAbsent.jsx
@@ -3,13 +3,13 @@ import { TableAbsentItem } from "./TableAbsentItem"
 export const TableAbsent = () => {
   return (
     <div>
-      <table className="table bg-white rounded-lg">
+      <table className="table bg-white rounded-lg ">
         <tr className="text-font-100 ">
-          <th className="font-medium">#</th>
-          <th className="font-medium">Name</th>
-          <th className="font-medium">Number</th>
-          <th className="font-medium">Attendance</th>
-          <th className="font-medium"></th>
+          <th className="font-medium hidden lg:block">#</th>
+          <th className="font-medium hidden lg:block">Name</th>
+          <th className="font-medium ">Number</th>
+          <th className="font-medium ">Attendance</th>
+          <th className="font-medium "></th>
         </tr>
         <TableAbsentItem/>
         <TableAbsentItem/>
--- frontend/admin/src/components/Classroom/Overview/TableAbsentItem.jsx
+++ frontend/admin/src/components/Classroom/Overview/TableAbsentItem.jsx
@@ -1,16 +1,19 @@
 export const TableAbsentItem = () => {
+
+
+
   return (
-    <tr className="">
-      <td>1</td>
-      <td>Ryan Manggala</td>
-      <td>12</td>
-      <td>
+    <tr className="text-base">
+      <td className="hidden lg:block">1</td>
+      <td className="hidden lg:block">Ryan Manggala</td>
+      <td className="">12</td>
+      <td className="">
         <span className="text-sm px-4 py-2 bg-danger/10 rounded-lg text-danger font-medium">
           Absent
         </span>
       </td>
       <td>
-        <button className="text-sm px-4 py-2 bg-primary text-white font-medium rounded-lg">Reason</button>
+        <button className="text-sm px-5 py-2 bg-primary text-white font-medium rounded-lg">Reason</button>
       </td>
     </tr>
   )
