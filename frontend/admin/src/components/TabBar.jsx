af95234e0402e8b7bbee5b0d321c05bb0639182d
aryndraa <aryacoder1102@gmail.com>
2025-02-09 22:32:15 +0800

    frontend - tab bar for desktop

--- frontend/admin/src/components/TabBar.jsx
+++ frontend/admin/src/components/TabBar.jsx
@@ -7,7 +7,7 @@ export const TabBar = () => {
 
   return (
     <>
-      <div className="flex justify-between relative items-center p-4 bg-white">
+      <div className="flex lg:hidden justify-between relative items-center p-4 bg-white">
         <div>
           <h2 className="text-xl font-medium text-font-300">Overview</h2>
         </div>
@@ -15,12 +15,12 @@ export const TabBar = () => {
           <button className="text-2xl" onClick={() => setOpenNav(!openNav)}><GoKebabHorizontal/></button>
         </div>
       </div>
-       <div className={`flex-col right-2 top-12 bg-white rounded-lg  ${
-        openNav ? "flex" : "hidden"
+       <div className={`flex-col lg:flex-row lg:gap-2 lg:p-4 bg-white rounded-lg  ${
+        openNav ? "flex" : "hidden lg:flex"
       }`}>
-        <Link to={'/'} className="py-3 px-4 font-medium text-font-100 hover:bg-neutral-100">Overview</Link>
-        <Link to={'/'} className="py-3 px-4 font-medium text-font-100 hover:bg-neutral-100">Subject Teachers</Link>
-        <Link to={'/'} className="py-3 px-4 font-medium text-font-100 hover:bg-neutral-100">List Students</Link>
+        <Link to={'/'} className="py-3 px-4 lg:py-2  font-medium text-font-100 hover:bg-neutral-100">Overview</Link>
+        <Link to={'/'} className="py-3 px-4 lg:py-2  font-medium text-font-100 hover:bg-neutral-100">Subject Teachers</Link>
+        <Link to={'/'} className="py-3 px-4 lg:py-2  font-medium text-font-100 hover:bg-neutral-100">List Students</Link>
       </div>
     </>
 
